f77909eac43626db5c3a1b4901b1ceeb0ad28b3c
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2024-12-03 11:31:38 +0100

    feat: add day02 tasks

---
+++ 2024/Day02/Solution.cs
@@ -0,0 +1,20 @@
+namespace AdventOfCode.Y2024.Day02;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+[ProblemName("Red-Nosed Reports")]
+class Solution : Solver {
+
+    public object PartOne(string input) {
+        return 0;
+    }
+
+    public object PartTwo(string input) {
+        return 0;
+    }
+}

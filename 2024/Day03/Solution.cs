f2d6476b1c6927e1814a4a280eeae8cc1d9f7965
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2024-12-03 13:04:43 +0100

    feat: add day03 part 1 solution

--- 2024/Day03/Solution.cs
+++ 2024/Day03/Solution.cs
@@ -6,12 +6,22 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
+using AngleSharp.Text;
 
 [ProblemName("Mull It Over")]
 class Solution : Solver {
 
     public object PartOne(string input) {
-        return 0;
+        string pattern = @"(?:mul\((?<x>[0-9]{1,3}),(?<y>[0-9]{1,3})\))";
+        var matches = Regex.Matches(input, pattern);
+        var total = 0;
+        foreach (Match mul in matches) {
+            var x = int.Parse(mul.Groups["x"].Value);
+            var y = int.Parse(mul.Groups["y"].Value);
+            //Console.WriteLine($"{x} * {y} = {x * y}");
+            total += x * y;
+        }
+        return total;
     }
 
     public object PartTwo(string input) {

ec11f03985fcb569d239d78d6ebdef33882b638f
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2024-12-03 12:24:18 +0100

    feat: add day02 solution

--- 2024/Day02/Solution.cs
+++ 2024/Day02/Solution.cs
@@ -11,10 +11,28 @@ using System.Text;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        return 0;
+        return ParseSamples(input).Count(Valid);
     }
 
     public object PartTwo(string input) {
-        return 0;
+        return ParseSamples(input).Count(levels => Dampener(levels).Any(Valid));
     }
+
+    IEnumerable<int[]> ParseSamples(string input) =>
+        from line in input.Split("\n")
+        let levels = line.Split(" ").Select(int.Parse)
+        select levels.ToArray();
+    
+    bool Valid(int[] levels) {
+        var pairs = Enumerable.Zip(levels, levels.Skip(1));
+        return 
+            pairs.All(p => 1 <= p.Second - p.First && p.Second - p.First <= 3) ||
+            pairs.All(p => 1 <= p.First - p.Second && p.First - p.Second <= 3);
+    }
+
+    IEnumerable<int[]> Dampener(int[] levels) =>
+        from i in Enumerable.Range(0, levels.Length+1)
+        let before = levels.Take(i - 1)
+        let after = levels.Skip(i)
+        select Enumerable.Concat(before, after).ToArray();
 }
